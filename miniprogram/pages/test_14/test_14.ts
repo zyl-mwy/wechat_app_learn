6ec61928586b32ee3d483a405d262b20535d14a6
zyl <linxi1909992592@163.com>
2022-07-07 23:08:22 +0800

    checkbox

---
+++ miniprogram/pages/test_14/test_14.ts
@@ -0,0 +1,90 @@
+// pages/test_14/test_14.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        id:0,
+        name:"苹果",
+        value:"apple"
+      },
+      {
+        id:1,
+        name:"葡萄",
+        value:"grape"
+      },
+      {
+        id:2,
+        name:"香蕉",
+        value:"banana"
+      }
+    ],
+    checkedList:[]
+  },
+  handleItemChange(e){
+    console.log(e);
+    const checkedList = e.detail.value;
+    this.setData({
+      checkedList
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
