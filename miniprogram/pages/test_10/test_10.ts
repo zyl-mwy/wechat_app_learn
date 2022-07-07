ab47cbebac1f2a7c6f87ce77a879643a586bf603
zyl <linxi1909992592@163.com>
2022-07-07 20:31:21 +0800

    rich_text

---
+++ miniprogram/pages/test_10/test_10.ts
@@ -0,0 +1,89 @@
+// pages/test_10/test_10.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    html_1:'<div class="rax-view-v2 FloorModule--tmcsModule--27mV5Wq" data-before-current-y="232.65625" data-appeared="true" data-has-appeared="true"><div data-spm-anchor-id="875.7931836.0.i1.2d504265AcQQrz"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/imgextra/i1/O1CN01obvh6l1iRpMLSol1z_!!6000000004410-2-tps-214-68.png_110x10000.jpg_.webp" data-before-current-y="769" data-appeared="true" data-has-appeared="true" style="width: 107px; height: 34px;"></div><div class="rax-view-v2 FloorModule--tmcsContent--16obPrv"><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=41466052023&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A1%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A41466052023%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i4/6000000004784/O1CN01BWtRB91lD7XNuptRA_!!6000000004784-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="634.65625" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">ABC卫生巾日用mini巾超薄棉柔迷你巾3包装护垫 带护翼防漏姨妈巾</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥13.9</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=15872507686&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A2%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A15872507686%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i3/6000000001235/O1CN01sqJv2V1KzfuQGHyG0_!!6000000001235-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="634.65625" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">云南白药牙膏留兰香型180g去黄牙垢去口臭口气清新减轻牙龈问题</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥32.9</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=612527302153&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A3%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A612527302153%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i3/6000000008024/O1CN018kxOWW2992nYzwoh5_!!6000000008024-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="634.65625" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">樱桃小丸子芒果味果冻100克*6大杯休闲食品小吃零食凑单网红爆款</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥11.9</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=525555365775&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A4%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A525555365775%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i3/6000000006456/O1CN01HMwoCb1xYtlcPV5v0_!!6000000006456-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="634.65625" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">蓝月亮洗衣液2kg组合装</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥29.9</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem1--1qW3uzm"><a href="//item.taobao.com/item.htm?id=628690864018&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A5%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A628690864018%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i2/6000000004934/O1CN013EYnD41mJowmlNlJg_!!6000000004934-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="187.328125" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">官方宜婴梦想家全包臀小内裤XXL34片超薄透气拉拉裤柔软尿不湿</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥79.2</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=566383591134&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A6%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A566383591134%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i3/6000000000661/O1CN01HDqRR11GkmZDPnvDd_!!6000000000661-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="632" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">洁柔手帕纸Face4层6片30包味小包纸巾超餐巾纸式面巾纸卫生纸随身</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥11.9</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=638473105092&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A7%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A638473105092%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i4/6000000007418/O1CN01Ov9PE224fUmJfeYp9_!!6000000007418-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="632" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">包邮润本电热蚊香液无味孕妇婴儿童驱蚊灭蚊液插电驱蚊水电蚊香</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥22.9</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=590947706179&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A8%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A590947706179%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i1/6000000001159/O1CN01PPgxjX1KQrnzLAYYS_!!6000000001159-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="632" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">世喜断奶仿真母乳奶嘴还原母乳实感新生婴儿超软宽口径防胀气1只</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥36</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem--c31nDTr"><a href="//item.taobao.com/item.htm?id=577829361993&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A9%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A577829361993%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i2/6000000002651/O1CN014gCSUl1VSCjhtck8L_!!6000000002651-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="632" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">七度空间优雅姨妈卫生巾日夜用组合装正品超薄官方店旗舰76片整箱</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥58.7</span></a></div><div class="rax-view-v2 FloorModule--tmcsContentItem1--1qW3uzm"><a href="//item.taobao.com/item.htm?id=615277548415&amp;scm=1007.12144.81309.73136_0_0&amp;pvid=45b23f3a-527b-4381-8f5a-f4dc1f71c400&amp;utparam=%7B%22x_hestia_source%22%3A%2273136%22%2C%22x_object_type%22%3A%22item%22%2C%22x_hestia_subsource%22%3A%22default%22%2C%22x_mt%22%3A8%2C%22x_src%22%3A%2273136%22%2C%22x_pos%22%3A10%2C%22wh_pid%22%3A-1%2C%22x_pvid%22%3A%2245b23f3a-527b-4381-8f5a-f4dc1f71c400%22%2C%22scm%22%3A%221007.12144.81309.73136_0_0%22%2C%22x_object_id%22%3A615277548415%2C%22tpp_buckets%22%3A%222144%230%2381309%230%22%7D"><img lazyload="true" quality="normal" data-once="true" src="//gw.alicdn.com/bao/uploaded/i1/6000000003515/O1CN012R2boS1bpuw2YOjd3_!!6000000003515-0-picassoopen.jpg_220x10000Q75.jpg_.webp" data-before-current-y="491.328125" data-appeared="true" data-has-appeared="true" style="width: 220px; height: 220px; border-radius: 18px;"><div class="FloorModule--mask--26Ahf9Q"></div><div class="FloorModule--tmcsName--19MBfMf">【包邮】Disney迪士尼正版婴儿枕头记忆棉防头偏儿童宝宝超软枕芯</div><span class="rax-text-v2 FloorModule--tmcsPrice--1dbgcpg">￥74.9</span></a></div></div></div>',
+    html:[
+      {
+        name:"div",
+
+        attrs:{
+          class:"my_div",
+          style:"color:red;"
+        },
+
+        children:[
+          {
+            name:"p",
+            attrs:{},
+            children:[
+              {
+                types:"text",
+                text:"hello"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
