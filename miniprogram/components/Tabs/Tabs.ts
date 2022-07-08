7153f8a0769fa257a8ae2c7344a847722cea4162
zyl <linxi1909992592@163.com>
2022-07-08 18:12:32 +0800

    activate

--- miniprogram/components/Tabs/Tabs.ts
+++ miniprogram/components/Tabs/Tabs.ts
@@ -11,13 +11,51 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    tabs:[
+      {
+        id: 0,
+        name: "首页",
+        isActive: true
+      },
+      {
+        id: 1,
+        name: "原创",
+        isActive: false
+      },
+      {
+        id: 2,
+        name: "分类",
+        isActive: false
+      },
+      {
+        id: 3,
+        name: "关于",
+        isActive: false
+      }
+    ]
   },
 
   /**
    * 组件的方法列表
    */
+  
   methods: {
+    handleItemTap(e){
+      //console.log("点击我试试");
+      console.log(e);
 
+      const {index} = e.currentTarget.dataset;
+
+      let {tabs} = this.data;
+      //let tabs=JSON.parse(JSON.stringify(this.data.tabs));
+      //let tabs=this.data.tabs;
+
+      tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+      //this.data.tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+
+      this.setData({
+        tabs
+      })
+    }
   }
 })
