a83d4ea2b785df8789ed5b1ac02b03fed54b5f94
zyl <linxi1909992592@163.com>
2022-07-09 17:54:38 +0800

    page life 2

---
+++ miniprogram/pages/test_16/test_16.ts
@@ -0,0 +1,81 @@
+// pages/test_16/test_16.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    console.log("onload");
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    console.log("onReady");
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    console.log("onshow");
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+    console.log("onHide");
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    console.log("onUnload");
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    console.log("onPullDownRefresh");
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+    console.log("onReachBottom");
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+    console.log("onShareAppMessage");
+  },
+  /**
+   *页面滚动
+   */
+  onPageScroll(){
+    console.log("onPageScroll");
+  },
+
+  onResize(){
+    console.log("onResize");
+  },
+
+  onTabItemTap(){
+    console.log("onTabItemTap");
+  }
+})
